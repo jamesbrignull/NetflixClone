591d403a1c1ad15810cf175565244811a41091cf
jamesbrignull <jamesbrignull@gmail.com>
2020-10-14 12:22:01 +0100

    jumbotron created

---
+++ netflix/src/components/jumbotron/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  Container,
+  Inner,
+  Item,
+  Pane,
+  Title,
+  SubTitle,
+  Image,
+} from "./styles/jumbotron";
+
+export default function Jumbrotron({
+  children,
+  direction = "row",
+  ...restProps
+}) {
+  return (
+    <Item direction={direction} {...restProps}>
+      <Inner>{children}</Inner>
+    </Item>
+  );
+}
+
+Jumbrotron.Container = function JumbotronContainer({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>;
+};
+
+Jumbrotron.Pane = function JumbotronPane({ children, ...restProps }) {
+  return <Pane {...restProps}>{children}</Pane>;
+};
+
+Jumbrotron.Title = function JumbotronTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
+
+Jumbrotron.SubTitle = function JumbotronSubTitle({ children, ...restProps }) {
+  return <SubTitle {...restProps}>{children}</SubTitle>;
+};
+
+Jumbrotron.Image = function JumbotronImage({ ...restProps }) {
+  return <Image {...restProps} />;
+};
