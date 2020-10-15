5fc2cf367599e0984620faa56b9964b720445fc1
jamesbrignull <jamesbrignull@gmail.com>
2020-10-15 10:55:28 +0100

    accordion finished

--- netflix/src/components/accordion/index.js
+++ netflix/src/components/accordion/index.js
@@ -29,7 +29,14 @@ Accordion.Item = function AccordionItem({ children, ...restProps}) {
 Accordion.Header = function AccordionHeader({ children, ...restProps}) {
     const { toggleShow, setToggleShow } = useContext(ToggleContext);
     
-    return <Header onClick={() => setToggleShow((toggleShow) => !toggleShow)} {...restProps}>{children}</Header>;
+    return <Header onClick={() => setToggleShow((toggleShow) => !toggleShow)} {...restProps}>
+                {children}
+                {toggleShow ? (
+                    <img src='/images/icons/close-slim.png' alt='Close' />
+                ) : (
+                    <img src='images/icons/add.png' alt='Open' />
+                )}
+            </Header>;
 }
 
 Accordion.Body = function AccordionBody({ children, ...restProps}) {
