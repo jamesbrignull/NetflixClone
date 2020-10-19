a922616c14b1fb7d1662bf8d5c81212c94a0a191
jamesbrignull <jamesbrignull@gmail.com>
2020-10-19 13:43:02 +0100

    headercomponent build

--- netflix/src/pages/home.js
+++ netflix/src/pages/home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { OptForm } from '../components';
+import { HeaderContainer } from '../containers/header';
 import { FaqsContainer } from "../containers/faqs";
 import { FooterContainer } from "../containers/footer";
 import { JumbotronContainer } from '../containers/jumbotron';
@@ -6,6 +8,14 @@ import { JumbotronContainer } from '../containers/jumbotron';
 export default function Home(){
     return (
         <>
+            <HeaderContainer>
+                <OptForm>
+                    <OptForm.Input placeholder='Email address' />
+                    <OptForm.Button>Try it now</OptForm.Button>
+                    <OptForm.Text> Ready to watch? Enter your emails to create or restart your membership</OptForm.Text>
+                </OptForm>
+            </HeaderContainer>
+
             <JumbotronContainer />
             <FaqsContainer />
             <FooterContainer />
