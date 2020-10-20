ac2bd0e3fd910e237ead24b4ec2aec1f57486bbb
jamesbrignull <jamesbrignull@gmail.com>
2020-10-20 17:20:10 +0100

    custom hooks for browse page

---
+++ netflix/src/hooks/use-content.js
@@ -0,0 +1,27 @@
+import { useEffect, useState, useContext } from 'react';
+import { FirebaseContext } from '../context/firebase';
+
+export default function useContext(target) {
+    const [content, setContent] = useState([]);
+    const { firebase } = useContext(FirebaseContext);
+
+    useEffect(() => {
+        firebase
+            .firestore()
+            .collection(target)
+            .get()
+            .then((snapsot) => {
+                const allContent = snapshot.docs.map((contentObj) => ({
+                    ...contentObj.data(),
+                    docId: contentObj.id,
+                }));
+
+                setContent(allContent);
+            })
+            .catch((error) => {
+                console.log(error.message);
+            });
+    }, [])
+
+    return { [target]: content };
+}
--- netflix/src/pages/browse.js
+++ netflix/src/pages/browse.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
 export default function Browse() {
+
+    
     return <p>Hello from sign broswe</p>
 }
