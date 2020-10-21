43f4f3c850597b15c18fe10a1d8392b81ee996d1
jamesbrignull <jamesbrignull@gmail.com>
2020-10-21 08:34:58 +0100

    profiles page established

--- netflix/src/containers/browse.js
+++ netflix/src/containers/browse.js
@@ -1,5 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { FirebaseContext } from '../context/firebase';
+import { SelectProfileContainer } from './profiles';
 
 export function BrowseContainer({ slides }) {
-    return <p>Hello!</p>
+
+  const { firebase } = useContext(FirebaseContext);
+  const user = firebase.auth().currentUser || {};
+
+  return (
+    <SelectProfileContainer user={user} />
+  );
 }
