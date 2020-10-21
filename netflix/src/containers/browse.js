c8c374d0c94a27ac4c2de44603f2b74e0ef02fbf
jamesbrignull <jamesbrignull@gmail.com>
2020-10-21 11:36:27 +0100

    loading spinner complete

--- netflix/src/containers/browse.js
+++ netflix/src/containers/browse.js
@@ -1,13 +1,33 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { FirebaseContext } from '../context/firebase';
 import { SelectProfileContainer } from './profiles';
+import { Header, Loading } from '../components'
 
 export function BrowseContainer({ slides }) {
-
+  const [profile, setProfile] = useState({});
+  const [loading, setLoading] = useState(true);
   const { firebase } = useContext(FirebaseContext);
   const user = firebase.auth().currentUser || {};
 
-  return (
-    <SelectProfileContainer user={user} />
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, [profile.displayName]);
+
+  return profile.displayName ? (
+
+    <>
+    {loading ? (
+        <Loading src={user.photoURL} /> 
+    ) : (
+        <Loading.ReleaseBody />
+    )}
+
+    <Header src="joker1"><p>Hello!</p></Header>
+    </>
+  ) : (  
+    <SelectProfileContainer user={user} setProfile={setProfile} />
   );
 }
